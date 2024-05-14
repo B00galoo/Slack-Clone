e0e49d319756e481ea2e0da9034ed427d5df2bef
B00galoo <coletresham24@gmail.com>
2024-05-13 22:10:52 -0400

    more work on nav

--- includes/nav.php
+++ includes/nav.php
@@ -9,24 +9,43 @@
                 Features
             </a>
             <ul class="dropdown-menu features-dropdown">
-                <div class="dropdown-container">
-                    <li class="dropdown-title">COLLABORATION</li>
-                    <li><a href="#" class="dropdown-item"><b>Channels</b><br>Organize teams and work</a></li>
-                    <li><a href="#" class="dropdown-item"><b>Slack Connect</b><br>Work with external partners</a></li>
-                    <li><a href="#" class="dropdown-item"><b>Messaging</b><br>Chat with your team</a></li>
-                    <li><a href="#" class="dropdown-item"><b>Huddles</b><br>Meet with audio and video</a></li>
-                    <li><a href="#" class="dropdown-item"><b>Clips</b><br>Record and share updates</a></li>
+                <div class="dropdown-section-container">
+                    <div class="dropdown-container">
+                        <li class="dropdown-title">COLLABORATION</li>
+                        <li><a href="#" class="dropdown-item"><b>Channels</b><br>Organize teams and work</a></li>
+                        <li><a href="#" class="dropdown-item"><b>Slack Connect</b><br>Work with external partners</a></li>
+                        <li><a href="#" class="dropdown-item"><b>Messaging</b><br>Chat with your team</a></li>
+                        <li><a href="#" class="dropdown-item"><b>Huddles</b><br>Meet with audio and video</a></li>
+                        <li><a href="#" class="dropdown-item"><b>Clips</b><br>Record and share updates</a></li>
+                    </div>
+                    <div class="dropdown-container">
+                        <li class="dropdown-title">Automation</li>
+                        <li><a href="#" class="dropdown-item"><b>Workflow Builder</b><br>Automate everyday tasks</a></li>
+                        <li><a href="#" class="dropdown-item"><b>Apps & Integration</b><br>Bring your tools to Slack</a></li>
+                        <li class="dropdown-title">Knowledge</li>
+                        <li><a href="#" class="dropdown-item"><b>Search</b><br>Seek shared knowledge</a></li>
+                        <li><a href="#" class="dropdown-item"><b>Canvas</b><br>Create rich, flexible docs</a></li>
+                        <li><a href="#" class="dropdown-item"><b>File Sharing</b><br>Bring files to the flow of work</a></li>
+                    </div> 
+                    <div class="dropdown-container">
+                        <li class="dropdown-title">Intelligence</li>
+                        <li><a href="#" class="dropdown-item"><span>&#9733;</span><b>Slack AI</b><br>Save time and work smarter with<br>powerfully simple AI</a></li>
+                        <li class="dropdown-title">Enterprise Platform</li>
+                        <li><a href="#" class="dropdown-item"><b>Security</b><br>Protect data, ensure compliance</a></li>
+                        <li><a href="#" class="dropdown-item"><b>Enterprise Key Management</b><br>Monitor and revoke access</a></li>
+                        <li><a href="#" class="dropdown-item"><b>Slack Atlas</b><br>Discover rich profiles and org<br>charts</a></li>
+                    </div>
+                    <div class="dropdown-container special-dropdown">
+                        <li class="dropdown-title">Featured</li>
+                        <img src="images/nav-img.png" alt="text">
+                        <li class="dropdown-text">Businesses of all sizes are working faster and smarter with Slack AI</li>
+                        <li><a href="#" class="dropdown-item">Learn more</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a href="#" class="dropdown-item"><b>What is Slack?</b></a></li>
+                        <li><a href="#" class="dropdown-item"><b>Slack vs. Email</b></a></li>
+                        <li><a href="#" class="dropdown-item"><b>Accessibility</b></a></li>
+                    </div>
                 </div>
-                <div class="dropdown-container">
-                    <li class="dropdown-title">Automation</li>
-                    <li><a href="#" class="dropdown-item"><b>Workflow Builder</b><br>Automate everyday tasks</a></li>
-                    <li><a href="#" class="dropdown-item"><d>Apps & Integration</d><br>Bring your tools to Slack</a></li>
-                    <li class="dropdown-title">Knowledge</li>
-                    <li><a href="#" class="dropdown-item"><b>Search</b><br>Seek shared knowledge</a></li>
-                    <li><a href="#" class="dropdown-item"><b>Canvas</b><br>Create rich, flexible docs</a></li>
-                    <li><a href="#" class="dropdown-item"><b>File Sharing</b><br>Bring files to the flow of work</a></li>
-                </div>
-                
             </ul>
         </li>
     </ul>
