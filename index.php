caf38afaab813db14e622e128b1ab726f18b0336
B00galoo <coletresham24@gmail.com>
2024-05-14 15:10:08 -0400

    ;lkj

--- index.php
+++ index.php
@@ -10,7 +10,22 @@
 <body>
     <?php include 'includes/nav.php' ?>
 
-    <h1>Test</h1>
+    <section class="cta">
+        <h1>Made for people. <span class="purple"> Built for productivity</span></h1>
+        <di class="cta-buttons">
+            <button id="email">SIGN UP WITH EMAIL</button>
+            <button id="google"><img src="images/google-logo.jpeg" alt="Google logo">SIGN UP WITH GOOGLE</button>
+        </div>
+        <p><b>Slack is free to try</b> for as long as you'd like</p>
+        <div class="cta-images">
+            <img src="images/logo-airbnb-small.png" alt="">
+            <img src="images/logo-nasa-small.png" alt="">
+            <img src="images/logo-uber-small.png" alt="">
+            <img src="images/logo-target-small.png" alt="">
+            <img src="images/logo-nyt-small.png" alt="">
+            <img src="images/logo-etsy-small.png" alt="">
+        </div>
+    </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
