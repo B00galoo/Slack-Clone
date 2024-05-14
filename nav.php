40ff428b89c6a2cc148bda90fd0fff1e1af12f2f
B00galoo <88115616+B00galoo@users.noreply.github.com>
2024-05-13 20:25:37 -0400

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,33 @@
+<nav class="navbar">
+    <a href="index.php" class="navbar-brand">
+        <img src="images/slack-logo.png" alt="Slack logo">
+    </a>
+
+    <ul class="navbar-nav" style="margin-right: 300px;">
+        <li class="nav-item dropdown">
+            <a href="#" role="button" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                Features
+            </a>
+            <ul class="dropdown-menu features-dropdown">
+                <div class="dropdown-container">
+                    <li class="dropdown-title">COLLABORATION</li>
+                    <li><a href="#" class="dropdown-item"><b>Channels</b><br>Organize teams and work</a></li>
+                    <li><a href="#" class="dropdown-item"><b>Slack Connect</b><br>Work with external partners</a></li>
+                    <li><a href="#" class="dropdown-item"><b>Messaging</b><br>Chat with your team</a></li>
+                    <li><a href="#" class="dropdown-item"><b>Huddles</b><br>Meet with audio and video</a></li>
+                    <li><a href="#" class="dropdown-item"><b>Clips</b><br>Record and share updates</a></li>
+                </div>
+                <div class="dropdown-container">
+                    <li class="dropdown-title">Automation</li>
+                    <li><a href="#" class="dropdown-item"><b>Workflow Builder</b><br>Automate everyday tasks</a></li>
+                    <li><a href="#" class="dropdown-item"><d>Apps & Integration</d><br>Bring your tools to Slack</a></li>
+                    <li class="dropdown-title">Knowledge</li>
+                    <li><a href="#" class="dropdown-item"><b>Search</b><br>Seek shared knowledge</a></li>
+                    <li><a href="#" class="dropdown-item"><b>Canvas</b><br>Create rich, flexible docs</a></li>
+                    <li><a href="#" class="dropdown-item"><b>File Sharing</b><br>Bring files to the flow of work</a></li>
+                </div>
+                
+            </ul>
+        </li>
+    </ul>
+</nav>
